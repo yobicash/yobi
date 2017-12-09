fda67e7c30b0b17976274a1e004c09a0a3e2290f
chritchens <chritchens@gmail.com>
2017-12-09 14:17:09 +0100

    Add network binary codecs

--- src/models/peer/mod.rs
+++ src/models/peer/mod.rs
@@ -14,10 +14,34 @@ pub struct YPeer {
 
 impl Default for YPeer {
     fn default() -> YPeer {
+        let ip = [0, 0, 0, 0];
+        let port = 2112;
+        YPeer::new(ip, port)
+    }
+}
+
+impl YPeer {
+    pub fn new(ip: [u8; 4], port: u16) -> YPeer {
         YPeer {
-            ip: [0, 0, 0 , 0],
-            port: 2112,
+            ip: ip,
+            port: port,
             last_time: YTime::now(),
         }
+    }
+
+    pub fn to_bytes(&self) -> YResult<Vec<u8>> {
+        unreachable!()
+    }
+
+    pub fn from_bytes(buf: &[u8]) -> YResult<YPeer> {
+        unreachable!()
+    }
+
+    pub fn to_hex(&self) -> YResult<String> {
+        unreachable!()
+    }
+
+    pub fn from_hex(s: &str) -> YResult<YPeer> {
+        unreachable!()
     }
 }
