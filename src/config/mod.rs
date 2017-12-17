fc45f6f64f997780dbb1d5ef1be39b03d8404af5
chritchens <chritchens@gmail.com>
2017-12-17 03:19:44 +0100

    Add price in configs

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -2,9 +2,8 @@ use libyobicash::utils::random::YRandom;
 use libyobicash::crypto::hash::sha::YSHA512;
 use libyobicash::transaction::YTransaction;
 use libyobicash::coinbase::YCoinbase;
-use errors::*;
-
 use models::peer::YPeer;
+use errors::*;
 
 #[derive(Clone, Eq, PartialEq, Debug)]
 pub struct YConfig {
@@ -12,6 +11,7 @@ pub struct YConfig {
     pub db_path: String,
     pub seed: Vec<YPeer>,
     pub port: u16,
+    pub price: u64,
 }
 
 impl Default for YConfig {
@@ -19,17 +19,19 @@ impl Default for YConfig {
         let password = YSHA512::hash(YRandom::bytes(64).as_slice()).to_hex();
         let seed = vec![YPeer::default()];
         let port = 2112;
+        let price = 0;
         YConfig {
             password: password,
             db_path: "TODO".to_string(), //TODO
             seed: seed,
             port: port,
+            price: price,
         }
     }
 }
 
 impl YConfig {
-    pub fn new(pswd: &str, db_path: &str, seed: &Vec<YPeer>, port: u16) -> YHResult<YConfig> {
+    pub fn new(pswd: &str, db_path: &str, seed: &Vec<YPeer>, port: u16, price: u64) -> YHResult<YConfig> {
         // TODO: check psdw complexity
         // TODO: check db_path
         // TODO: check seed peers
@@ -38,6 +40,7 @@ impl YConfig {
             db_path: String::from(db_path),
             seed: seed.clone(),
             port: port,
+            price: price,
         })
     }
 
